5a9b17d84f7f646153103bb9d50185732ef189f3
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2026-02-26 03:07:04 -0500

    Created enhanced fastq report creation tool

--- FASTQ/create_fastqc_files.sh
+++ FASTQ/create_fastqc_files.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env bash
 
+input_dir=""
 output_dir=""
 
-while getopts "o:" opt; do
+print_usage() {
+  echo "Usage: $0 -i input_dir -o output_dir" >&2
+}
+
+while getopts "i:o:" opt; do
   case "$opt" in
+    i) input_dir="$OPTARG" ;;
     o) output_dir="$OPTARG" ;;
-    # The below case is a wildcard cases that matches everything
-    \?) echo "Usage: $0 -o output_dir" >&2; exit 1 ;;
+    # The below case is a wildcard case that matches everything
+    \?) print_usage; exit 1 ;;
   esac
 done
 
 # -z checks if the string is empty
-if [[ -z "$output_dir" ]]; then
+if [[ -z "$input_dir" || -z "$output_dir" ]]; then
   # >&2 redirects output to stderr
-  echo "Usage: $0 -o output_dir" >&2
+  print_usage
   exit 1
 fi
 
@@ -21,4 +27,9 @@ fi
 mkdir -p "$output_dir"
 
 # ::: is shell globbing
-parallel --progress -j 4 fastqc -o "$output_dir" ::: FASTQ/*.fastq
+for file in "$input_dir"/*.fastq; do
+  printf "%s|%s\n" "$output_dir" "$file"
+done | parallel --progress -j 4 '
+  IFS="|" read -r output_dir input_file <<< {}
+  fastqc -o "$output_dir" "$input_file"
+'
