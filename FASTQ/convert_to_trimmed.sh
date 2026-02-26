e70009107a51690e8c4b4c6737bcc0cb938d9367
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2026-02-26 04:37:03 -0500

    Consolidated files

--- FASTQ/convert_to_trimmed.sh
+++ FASTQ/convert_to_trimmed.sh
@@ -1,18 +1,46 @@
 #!/usr/bin/env bash
 
-output_dir="../Trimmed FASTQ"
+input_dir=""
+output_dir=""
+
+print_usage() {
+  echo "Usage: $0 -i input_dir -o output_dir" >&2
+}
+
+while getopts "i:o:" opt; do
+  case "$opt" in
+    i) input_dir="$OPTARG" ;;
+    o) output_dir="$OPTARG" ;;
+    # The below case is a wildcard case that matches everything
+    \?) print_usage; exit 1 ;;
+  esac
+done
+
+# -z checks if the string is empty
+if [[ -z "$input_dir" || -z "$output_dir" ]]; then
+  # >&2 redirects output to stderr
+  print_usage
+  exit 1
+fi
+
+# -p causes mkdir to not error and create parents if needed
+export output_dir
 mkdir -p "$output_dir"
 
+# Fix script to be more flexible in choosing an adapter
+cd "$input_dir"
 # sed is a stream editor, in this command, the format is s/old/new/, meaning substitute old with new.
 ls *_R1.fastq \
   | sed 's/_R1\.fastq$//' \
   | parallel --progress -j 2 '
-      R1={}_R1
-      R2={}_R2
-      trimmomatic PE -threads 16 -phred33 "${R1}.fastq" "${R2}.fastq" \
-        "'"$output_dir"'/${R1}.paired.fastq" \
-        "'"$output_dir"'/${R1}.unpaired.fastq" \
-        "'"$output_dir"'/${R2}.paired.fastq" \
-        "'"$output_dir"'/${R2}.unpaired.fastq" \
-        ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+      # basename {1} gives just the filename
+      BASE=$(basename {1})
+      
+      trimmomatic PE -threads 8 -phred33 \
+      "{1}_R1.fastq" "{1}_R2.fastq" \
+      "$output_dir/${BASE}_R1.paired.fastq" \
+      "$output_dir/${BASE}_R1.unpaired.fastq" \
+      "$output_dir/${BASE}_R2.paired.fastq" \
+      "$output_dir/${BASE}_R2.unpaired.fastq" \
+      ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
   '
--- FASTQ/download_files.sh
+++ FASTQ/download_files.sh
@@ -2,6 +2,33 @@
 
 # This script requires GNU parallel (https://www.gnu.org/software/parallel/) to be installed
 # Got SRR ids from: https://www.ncbi.nlm.nih.gov/sra/?term=SRP263458
+
+output_dir=""
+
+print_usage() {
+  echo "Usage: $0 -o output_dir" >&2
+}
+
+while getopts "o:" opt; do
+  case "$opt" in
+    o) output_dir="$OPTARG" ;;
+    # The below case is a wildcard case that matches everything
+    \?) print_usage; exit 1 ;;
+  esac
+done
+
+# -z checks if the string is empty
+if [[ -z "$output_dir" ]]; then
+  # >&2 redirects output to stderr
+  print_usage
+  exit 1
+fi
+
+# -p causes mkdir to not error and create parents if needed
+export output_dir
+mkdir -p "$output_dir"
+
+
 input_prefix="SRR"
 declare -A experiment_groups=(
   [Experiment1]="$(printf '%s ' ${input_prefix}{11841493..11841494})"
@@ -12,7 +39,7 @@ declare -A experiment_groups=(
 
 process_file() {
   read -r key val replicate_number <<< "$1"
-  save_file="${key}_Replicate${replicate_number}.fastq"
+  save_file="$output_dir/${key}_Replicate${replicate_number}.fastq"
   printf "Creating %s...\n" "$save_file"
   
   # Create temp directory and use trap to ensure that fasterq create directories are always cleaned up
@@ -39,3 +66,14 @@ for key in "${!experiment_groups[@]}"; do
     replicate_number=$((replicate_number+1))
   done
 done | parallel --progress -j 4 process_file {}
+
+# Renaming the files to have R1 & R2 at the end to be more clear
+for file in "$output_dir"/*_1.fastq; do
+  new_name="${file%_1.fastq}_R1.fastq"
+  mv "$file" "$new_name"
+done
+
+for file in "$output_dir"/*_2.fastq; do
+  new_name="${file%_2.fastq}_R2.fastq"
+  mv "$file" "$new_name"
+done
